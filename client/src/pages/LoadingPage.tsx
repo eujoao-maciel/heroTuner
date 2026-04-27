d8ace0fcc71d0369b1e678ac63d1693a762438e4
notebook da lesli <joao.eudes13jem@gmail.com>
2026-04-27 01:27:36 -0300

    feat: implement initial loading screen layout

--- client/src/pages/LoadingPage.tsx
+++ client/src/pages/LoadingPage.tsx
@@ -1,14 +1,22 @@
 import background from "../assets/background.png"
+import herotuner from '../assets/herotuner.png'
+import './styles/animations.css'
 
 export const LoadingPage = () => {
     console.log(background)
 
     return (
-        <main>
+        <main className='relative'>
             <img
                 src={background}
                 alt="Dark industrial metal background with a repeating pattern of skulls, pentagrams, and lightning bolts."
-                className="w-full h-screen object-cover"
+                className="w-full h-screen object-cover z-0"
+            />
+
+            <img 
+               src={herotuner}
+               alt='herotuner loading logo'
+               className='absolute inset-0 m-auto z-50 animate-hero-pulse'
             />
         </main>
     )
