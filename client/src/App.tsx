63c44d61ca605ca6dd373ee15c1a0233f0ad2775
notebook da lesli <joao.eudes13jem@gmail.com>
2026-04-30 16:42:12 -0300

    feat: add default tuner page and integrate layout with react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
-import { LoadingPage } from './pages/LoadingPage'
+import { Routes, Route } from "react-router-dom"
+import { Layout } from "./layout/Layout.tsx"
+import { LoadingPage } from "./pages/LoadingPage"
+import { TunerPage } from "./pages/TunerPage"
 
 function App() {
-  return (
-    <>
-        <LoadingPage />
-   </>
-  )
+    return (
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<LoadingPage />} />
+                <Route path="/tuner" element={<TunerPage />} />
+            </Route>
+        </Routes>
+    )
 }
 
 export default App
--- client/src/pages/LoadingPage.tsx
+++ client/src/pages/LoadingPage.tsx
@@ -1,25 +1,14 @@
-import background from "../assets/background.webp";
-import herotuner from "../assets/herotuner.webp";
-import "./styles/animations.css";
+import herotuner from "../assets/herotuner.webp"
+import "./styles/animations.css"
 
 export const LoadingPage = () => {
-  return (
-    <main className="relative w-full h-screen bg-[var(--color-bg-primary)] overflow-hidden">
-      <img
-        src={background}
-        alt="background"
-        className="absolute inset-0 w-full h-full object-cover"
-      />
-
-      <div className="absolute inset-0 bg-black/40" />
-
-      <div className="absolute inset-0 flex items-center justify-center z-10">
-        <img
-          src={herotuner}
-          alt="herotuner loading logo"
-          className="animate-hero-pulse"
-        />
-      </div>
-    </main>
-  );
-};
+    return (
+        <div className="flex items-center justify-center">
+            <img
+                src={herotuner}
+                alt="herotuner loading logo"
+                className="animate-hero-pulse"
+            />
+        </div>
+    )
+}
