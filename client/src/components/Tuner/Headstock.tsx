441f3378cc583c605ccea70b62fb1ef9c6551805
j <joao.eudes13jem@gmail.com>
2026-06-23 05:47:51 -0300

    fix: improve note alignment in mobile and desktop layouts

--- client/src/components/Tuner/Headstock.tsx
+++ client/src/components/Tuner/Headstock.tsx
@@ -2,17 +2,24 @@ import headstock from "../../assets/headstock.webp";
 
 export const Headstock = ({ children }: { children?: React.ReactNode }) => {
   return (
-    <div className="relative w-[220px] h-[420px] md:w-[520px] md:h-[280px]">
+    <div className="relative w-[370px] h-[400px] md:h-[320px]">
       <img
         src={headstock}
         alt="headstock"
         className="
           absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-          w-[320px] h-[160px] -rotate-90
-          md:w-[420px] md:h-[210px] md:rotate-0
+          w-[420px] h-auto -rotate-90 md:rotate-0
         "
       />
-      {children}
+      <div
+        className="
+          absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+          w-[160px] h-[320px]
+          md:w-[420px] md:h-[210px]
+        "
+      >
+        {children}
+      </div>
     </div>
   );
 };
